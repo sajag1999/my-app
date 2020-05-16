4b3c10832bc8a12748be50bc393aa88fce0aaffb
sajagchauhan <love.ambani.7@gmail.com>
2020-05-16 14:46:12 +0530

    Updates

---
+++ precache-manifest.55aa2ab12eea2b2452cb6ab82c5885ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d481a0321c7fbb53f4431c042b65012",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "566f5f03751957f1482c",
+    "url": "/my-app/static/css/main.2125faea.chunk.css"
+  },
+  {
+    "revision": "080787be79365fdaa2cc",
+    "url": "/my-app/static/js/2.ee379888.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.ee379888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "566f5f03751957f1482c",
+    "url": "/my-app/static/js/main.bedf1336.chunk.js"
+  },
+  {
+    "revision": "50088aaeaec02b9ce465",
+    "url": "/my-app/static/js/runtime-main.fd6f11c2.js"
+  }
+]);
